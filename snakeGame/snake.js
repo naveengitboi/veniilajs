266d5058d54784ce46ba0b3d2c3e789562ce913d
naveengitboi <naveenjangiti9@gmail.com>
2024-03-31 17:32:23 +0530

    added some more code with error

--- snakeGame/snake.js
+++ snakeGame/snake.js
@@ -1,85 +1,121 @@
 const border = document.querySelector('.snakeInnerBorder');
 const snakeInnerBorder = document.querySelector('.snakeInnerBorder')
 const snakeHead = document.querySelector('.snakeHead')
+const instructionText = document.querySelector('.instructions')
 const gridSize = 20
 let direction = 'right'
+let gameInterval;
+let gameStarted= false;
 let snakeLen = [
     {
-        x: 200,
-        y: 200
-    },
-    {
-        x: 60,
-        y: 60
+        x: 10,
+        y: 10
     }
 ]
-const snakeFoodPosition = generateRandom()
+let sfPosition;
 //random position
-function generateRandom() {
-    const foodPosition = Math.floor(Math.random() * gridSize)
-    console.log(foodPosition)
-    return foodPosition;
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize)
+    const y = Math.floor(Math.random() * gridSize)
+    return { x, y }
+}
+
+function draw() {
+    snakeInnerBorder.innerHTML = ''
+    drawSnake();
+    drawSnakeFood()
+}
+
+function drawSnake() {
+    snakeLen.forEach((segment) => {
+        const snakePart = createPart('div', 'snakeHead')
+
+        setPosition(snakePart, segment)
+        snakeInnerBorder.appendChild(snakePart)
+    })
+}
+
+function drawSnakeFood() {
+    const foodEle = createPart('div', 'snakeFood')
+    sfPosition = generateFood()
+    setPosition(foodEle, sfPosition)
+    snakeInnerBorder.appendChild(foodEle)
+
+}
+
+function createPart(ele, classVal) {
+    const part = document.createElement(ele);
+    part.className = classVal
+    return part;
+}
+function setPosition(ele, position) {
+    ele.style.gridColumn = position.y
+    ele.style.gridRow = position.x
 }
 
 //move snakehead
 function moveSnake() {
-    switch (keyButton){
-        case 'ArrowUp':
-            direction = 'up'
-            break
-        case 'ArrowDown':
-            direction = 'down'
-            break
-        case 'ArrowLeft':
-            direction = 'left'
-            break
-        case 'ArrowRight':
-            direction = 'right'
-            break
-    }
-
-    const moveSide = snakeHead.style
-
-    switch (direction){
+    const head = { ...snakeHead[0] }
+    switch (direction) {
         case 'right':
-            moveSide.left = snake
+            head.x++;
             break
-        case 'ArrowDown':
-            direction = 'down'
+        case 'left':
+            head.x--;
             break
-        case 'ArrowLeft':
-            direction = 'left'
+        case 'up':
+            head.y--
             break
-        case 'ArrowRight':
-            direction = 'right'
+        case 'down':
+            head.y++
             break
     }
 
-}
+    snakeLen.unshift(head)
 
-function createSnakePart() {
-    const part = document.createElement('div');
-    part.className = "snakeHead"
-    part.style.top = snakeLen[0].y + 'px'
-    part.style.left = snakeLen[0].x + 'px'
-    snakeInnerBorder.appendChild(part)
-
-}
-
-function gameStart() {
-    const interval = setInterval(() => {
-        window.addEventListener('keydown', () => {
+    if (head.x == sfPosition.x && head.y == sfPosition.y) {
+        food = generateFood();
+        clearInterval();
+        gameInterval = setInterval(() => {
             moveSnake();
-        })
-    }, 100)
-    clearInterval(interval)
+            draw()
+        },100)
+    } else {
+        snakeLen.pop()
+    }
+
 }
 
-
-window.addEventListener('keydown', (e) => {
-    if (e.key == 'Space' || e.key == ' ') {
-        gameStart()
+function startGame(){
+    gameStarted = true
+    instructionText.style.display = 'none';
+    clearInterval(gameInterval)
+    gameInterval = setInterval(() => {
+        moveSnake();
+        draw()
+    })
+}
+const handleKeyPress = (e) => {
+    if(e.code == 'Space' || e.key == ' '){
+        startGame()
     }
-})
+    else{
+        switch (e.key) {
+            case 'ArrowUp':
+                direction = 'up'
+                break
+            case 'ArrowDown':
+                direction = 'down'
+                break
+            case 'ArrowLeft':
+                direction = 'left'
+                break
+            case 'ArrowRight':
+                direction = 'right'
+                break
+        }
 
-//game controls
+    }
+}
+
+window.addEventListener('keydown', handleKeyPress)
