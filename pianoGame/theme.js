e03ab402bb1bacc54d15ec0cb5c4752e0b705129
naveengitboi <naveenjangiti9@gmail.com>
2025-03-27 22:52:50 +0530

    added custom theme selection option

--- pianoGame/theme.js
+++ pianoGame/theme.js
@@ -1,5 +1,13 @@
 const root = document.documentElement;
 const themesInputEle = document.querySelectorAll('.themeSelection .themeCode input')
+const themesSelectionEle = document.querySelector('.themeSelection')
+const colorControlEle = document.querySelector('.colorControls .circle')
+
+colorControlEle.addEventListener('click', () =>{
+  colorSwitch = true;
+  colorControlEle.classList.add('circleActive');
+  themesSelectionEle.classList.add('themeSelectionActive')
+})
 
 const themes = [
   {
