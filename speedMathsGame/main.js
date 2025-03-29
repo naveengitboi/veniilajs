79fee3ae9f0aa3d1444360fce8c2f8e176228c17
naveengitboi <naveenjangiti9@gmail.com>
2025-03-29 17:39:07 +0530

    speed maths game

--- speedMathsGame/main.js
+++ speedMathsGame/main.js
@@ -4,6 +4,7 @@ const firstNum = document.querySelector(".firstNumber");
 const operator = document.querySelector(".operator");
 const secondNum = document.querySelector(".secondNumber");
 const inputAnswer = document.querySelector(".answer");
+const newGame = document.querySelector(".newGame");
 
 let operatorSymbol = ["*", "+", "-", "/"];
 let points = 0;
@@ -17,10 +18,15 @@ function getRandomNumber(minRange, maxRange) {
 
 function getValues(minRange, maxRange) {
   let ope = operatorSymbol[getRandomNumber(0, 3)];
-  let fnum = getRandomNumber(1, 20);
-  let snum = getRandomNumber(1, 20);
-  while ((ope == "/" || ope == "-") && fnum < snum) {
-    snum = getRandomNumber(1, 20);
+  let fnum = getRandomNumber(minRange, maxRange);
+  let snum = getRandomNumber(minRange, maxRange);
+  if(ope == "-") {
+    fnum = Math.max(fnum, snum);
+    snum = Math.min(fnum, snum);
+  }
+  if(ope == "/"){
+    let multiplier = getRandomNumber(1, 9);
+    fnum = snum*multiplier;
   }
   return [fnum, snum, ope];
 }
@@ -37,8 +43,7 @@ function getTarget(fnum, snum, ope) {
   } else {
     target = fnum / snum;
   }
-  target = Math.round(target, 2);
-
+  target = Math.round(target);
   return target;
 }
 
@@ -50,27 +55,37 @@ function updateScore(val) {
 
 function moveToNextQuestion() { }
 
-function checkAnswer(e) {
+function checkAnswer(e, fnum, snum, ope) {
+  console.log(fnum, snum, ope);
   let target = getTarget(fnum, snum, ope);
+  console.log(target);
   let val = e.target.value;
   if (val == target) {
-    moveToNextQuestion();
-    updateScore(points++);
+    updateScore(++points);
+    inputAnswer.value = "";
+    playGame();
   }
 }
 
 function playGame() {
-  let [fnum, snum, ope] = getValues(1, 20);
+  let [fnum, snum, ope] = getValues(1, 30);
   firstNum.innerText = fnum;
   secondNum.innerText = snum;
   operator.innerText = ope;
-  inputAnswer.addEventListener("change", checkAnswer);
+  inputAnswer.addEventListener("change", (e) =>{
+    checkAnswer(e, fnum, snum, ope);
+  });
 }
 
 let steps = 10;
-while(steps > 0)
-{
-  playGame();
+playGame();
 
+function resetGame(){
+  points = 0;
+  updateScore(points);
 }
+
+newGame.addEventListener("click", resetGame);
+
+
 
